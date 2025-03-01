d0f1a409933d14aaa393ed10c3ab89badc55202d
AnhViet5523 <vietnase180540@fpt.edu.vn>
2025-03-02 00:46:42 +0700

    PagesOfFooter
    PagesOfFooter

--- shopco-main/src/App.jsx
+++ shopco-main/src/App.jsx
@@ -15,7 +15,12 @@ import DaKho from "./pages/CareProcess/DaKho/DaKho";
 import DaThuong from "./pages/CareProcess/DaThuong/DaThuong";
 import DaHonHop from "./pages/CareProcess/DaHonHop/DaHonHop";
 import DaNhayCam from "./pages/CareProcess/DaNhayCam/DaNhayCam";
-
+import Intro from "./pages/PagesOfFooter/Intro";
+import Buy from "./pages/PagesOfFooter/Buy";
+import Term from "./pages/PagesOfFooter/Term";
+import PrivacyPolicy from "./pages/PagesOfFooter/Policy";
+import Complaint from "./pages/PagesOfFooter/Complaint";
+import Return from "./pages/PagesOfFooter/Return";
 
 export default function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -89,6 +94,12 @@ export default function App() {
         <Route path="/da-thuong" element={<DaThuong />} />
         <Route path="/da-hon-hop" element={<DaHonHop />} />
         <Route path="/da-nhay-cam" element={<DaNhayCam />} />
+        <Route path="/intro" element={<Intro />} />
+        <Route path="/buy" element={<Buy />} />
+        <Route path="/term" element={<Term />} />
+        <Route path="/policy" element={<PrivacyPolicy />} />
+        <Route path="/complaint" element={<Complaint />} />
+        <Route path="/return" element={<Return />} />
       </Routes>
     </BrowserRouter>
   );
