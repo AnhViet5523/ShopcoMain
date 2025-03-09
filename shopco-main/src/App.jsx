182c9dacb18a0f5855bbcd15e590fb8aecf5f8d2
DamThanQuy <qdam100@gmail.com>
2025-03-09 21:26:56 +0700

    fix app jsx

--- shopco-main/src/App.jsx
+++ shopco-main/src/App.jsx
@@ -50,7 +50,6 @@ function NavigationHandler() {
   return null;
 }
 
-
 export default function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
@@ -85,45 +84,10 @@ export default function App() {
   };
 
   return (
-
-    <BrowserRouter>
-      <Routes>
-        {/* Public Routes - Ai cũng truy cập được */}
-        <Route
-          path="/"
-          element={<MainScreen onSignOut={handleSignOut} />}
-        />
-        <Route path="/product/:id" element={<ProductScreen />} />
-        <Route path="/search" element={<SearchResults />} />
-        <Route path="/category" element={<CategoryScreen />} />
-        <Route path="/categories" element={<CategoryContent />} />
-        <Route path="/categories/:id" element={<CategoryContent />} />
-        <Route path="/brand/:brandName" element={<BrandProducts />} />
-        
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/blog1" element={<Blog1 />} />
-        <Route path="/blog2" element={<Blog2 />} />
-        <Route path="/blog3" element={<Blog3 />} />
-        <Route path="/blog4" element={<Blog4 />} />
-        <Route path="/blog5" element={<Blog5 />} />
-        <Route path="/blog6" element={<Blog6 />} />
-
-        <Route path="/da-dau" element={<DaDau />} />
-        <Route path="/da-kho" element={<DaKho />} />
-        <Route path="/da-thuong" element={<DaThuong />} />
-        <Route path="/da-hon-hop" element={<DaHonHop />} />
-        <Route path="/da-nhay-cam" element={<DaNhayCam />} />
-        <Route path="/intro" element={<Intro />} />
-        <Route path="/buy" element={<Buy />} />
-        <Route path="/term" element={<Term />} />
-        <Route path="/policy" element={<PrivacyPolicy />} />
-        <Route path="/complaint" element={<Complaint />} />
-        <Route path="/return" element={<Return />} />
     <ErrorBoundary>
       <BrowserRouter>
         {/* Component để hủy request khi chuyển trang */}
         <NavigationHandler />
-
         
         <Routes>
           {/* Public Routes */}
@@ -135,6 +99,14 @@ export default function App() {
           <Route path="/categories/:id" element={<CategoryContent />} />
           <Route path="/brand/:brandName" element={<BrandProducts />} />
           <Route path="/quiz" element={<QuizTest />} />
+          
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog1" element={<Blog1 />} />
+          <Route path="/blog2" element={<Blog2 />} />
+          <Route path="/blog3" element={<Blog3 />} />
+          <Route path="/blog4" element={<Blog4 />} />
+          <Route path="/blog5" element={<Blog5 />} />
+          <Route path="/blog6" element={<Blog6 />} />
 
           {/* Static Pages */}
           <Route path="/da-dau" element={<DaDau />} />
